54c52fa0bd49ff438076a582b675ab83afeb4467
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-26 15:41:29 -0400

    MVP met

--- index.js
+++ index.js
@@ -6,10 +6,77 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+
+const knex = require('knex');
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+}
+const db = knex(knexConfig);
+
 // endpoints here
 
+      //basic endpoint
 server.get('/', (req,res) => {
   res.send('im working');
+})
+
+      //post 
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(ids => {
+      const id = ids[0]
+      db('zoos').where({ id: id })
+        .first()
+        .then(zoo => {
+          res.status(200).json(zoo)
+        })
+        .catch(err => res.status(500).json(err))
+    })
+    .catch(err => res.status(500).json(err))
+})
+      //baseline get request
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json(err))
+})
+      //get specific zoo
+server.get('/api/zoos/:id', (req, res) => {
+  const id = req.params.id;
+  db('zoos').where({id: id})
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err))
+})
+      //delete by specific id
+server.delete('/api/zoos/:id', (req,res) => {
+  const id = req.params.id;
+
+  db('zoos').where({ id: id }).del()
+    .then(count => {
+      if(count > 0 ){
+        res.status(204).json("successfully deleted item")
+      } else {
+        res.status(404).json({message: 'couldnt delete this item'})
+      }
+    })
+    .catch(err => res.status(500).json(err))
+})
+
+server.put('/api/zoos/:id', (req,res) => {
+  db('zoos').where({ id: req.params.id }).update(req.body)
+    .then(count => {
+      if(count > 0 ){
+        res.status(201).json(count)
+      }else{
+        res.status(404).json({message: 'zoo not found'})
+      }
+    })
+    .catch(err => res.status(500).json(err))
 })
 
 
